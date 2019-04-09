d3289b4f34f30ff754373371f4aa997d5e45ff07
Amir <amiromayer@gmail.com>
2019-04-09 10:42:46 +0200

    Refactored interfaces

---
+++ contracts/contracts/interfaces/ILinkdrop.sol
@@ -0,0 +1,54 @@
+
+interface ILinkdrop {
+
+    event Canceled(address linkId, uint timestamp);
+    event Claimed(address indexed linkId, uint amount, address receiver, uint timestamp);
+
+    function verifySenderSignature
+    (
+        address _token,
+        uint _amount,
+        address _linkId,
+        uint _expiration,
+        bytes calldata _signature
+    )
+    external view returns (bool);
+
+    function verifyReceiverSignature
+    (
+        address _linkId,
+	    address _receiver,
+		bytes calldata _signature
+    )
+    external view returns (bool);
+
+    function checkClaimParams
+    (
+        address _token,
+        uint _amount,
+        address _linkId, 
+        uint _expiration,
+        bytes calldata _senderSignature,
+        address _receiver, 
+        bytes calldata _receiverSignature
+    )
+    external view returns (bool);
+
+    function claim
+    (
+        address _token, 
+        uint _amount,
+        address _linkId, 
+        uint _expiration,
+        bytes calldata _senderSignature, 
+        address _receiver, 
+        bytes calldata _receiverSignature
+    ) 
+    external returns (bool);
+
+    function isClaimedLink(address _linkId) external view returns (bool);
+    function cancel(address _linkId) external returns (bool);
+    
+}
+
+// ${host}/#/claim?t=${tokenAddress}&a=${claimAmount}&exp=${expirationTime}&pk=${linkKey}&sig=${senderSignature}&c=${contractAddress}
