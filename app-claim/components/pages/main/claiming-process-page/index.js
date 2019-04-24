a332fce14eade12e964b9cb8dcdc38bffb012d3e
Haz Baykulov <spacehaz@gmail.com>
2019-04-24 16:35:57 +0300

    added translations for comments

--- app-claim/components/pages/main/claiming-process-page/index.js
+++ app-claim/components/pages/main/claiming-process-page/index.js
@@ -12,10 +12,10 @@ class ClaimingProcessPage extends React.Component {
   componentDidMount () {
     const { wallet } = this.props
     const { token, amount, expirationTime } = getHashVariables()
-    // destination: destination address - это адрес получается, из стора
-    // token: ERC20 token address, 0x000...000 for ether - это из линка
-    // tokenAmount: token amount in atomic values - это из линка
-    // expirationTime: link expiration time - это из линка
+    // destination: destination address - can be received from web3-react context
+    // token: ERC20 token address, 0x000...000 for ether - can be received from url params
+    // tokenAmount: token amount in atomic values - can be received from url params
+    // expirationTime: link expiration time - can be received from url params
     this.actions().tokens.claimTokens({ wallet, token, tokenAmount: amount, expirationTime })
   }
 
--- app-claim/components/pages/main/index.js
+++ app-claim/components/pages/main/index.js
@@ -24,27 +24,27 @@ import { Web3Consumer } from 'web3-react'
 class Claim extends React.Component {
   componentDidMount () {
     const { token, amount, expirationTime, n } = getHashVariables()
-    // так, вот есть переменные все в урле:
-    // token - это адрес контракта,
-    // amount - количество токенов,
-    // expirationTime - дата окончания дествия линка,
+    // params in url:
+    // token - contract/token address,
+    // amount - tokens amount,
+    // expirationTime - expiration time of link,
     // sender,
     // senderSignature,
-    // linkKey - приватный ключ для линка,
-    // n - айдишник сети
+    // linkKey - private key for link,
+    // n - network id
 
-    // нужно для клейма
+    // params needed for claim
     // sender: sender key address, e.g. 0x1234...ff
     // senderSignature: ECDSA signature signed by sender (contained in claim link)
     // receiverSignature: ECDSA signature signed by receiver using link key
 
-    // destination: destination address - это адрес получается, из стора
-    // token: ERC20 token address, 0x000...000 for ether - это из линка
-    // tokenAmount: token amount in atomic values - это из линка
-    // expirationTime: link expiration time - это из линка
+    // destination: destination address - can be received from web3-react context
+    // token: ERC20 token address, 0x000...000 for ether - can be received from url params
+    // tokenAmount: token amount in atomic values - can be received from url params
+    // expirationTime: link expiration time - can be received from url params
 
     if (Number(expirationTime) < +(new Date())) {
-      // если ссылка просрочилась, то показывать экран ошибки что ссылка исчерпалась уже
+      // show error page if link expired
       return this.actions().user.setErrors({ errors: ['LINK_EXPIRED'] })
     }
     this.actions().contract.getTokenData({ tokenAddress: token, amount, networkId: n })
@@ -71,6 +71,7 @@ class Claim extends React.Component {
     } = context
     const commonData = { decimals, amount, symbol, icon, wallet: account, loading: userLoading }
     if (errors && errors.length > 0) {
+      // if some errors occured and can be found in redux store, then show error page
       return <ErrorPage error={errors[0]} />
     }
     switch (step) {
@@ -79,20 +80,20 @@ class Claim extends React.Component {
           {...commonData}
           onClick={_ => {
             if (account) {
-              // если уже есть кошелек, то перекидываем на четвертый шаг и клеймим токены там уже
-              return this.actions().user.setStep({ step: 2 })
+              // if wallet account was found in web3 context, then go to step 4 and claim data
+              return this.actions().user.setStep({ step: 4 })
             }
-            // если нет кошелька в сторе сейчас, то перекидываем на страницу с выбором кошелька
+            // if wallet was not found in web3 context, then go to step 2 with wallet select page and instructions
             this.actions().user.setStep({ step: 2 })
           }}
         />
       case 2:
-        // страница выбора кошелька
+        // page with wallet select component
         return <WalletChoosePage onClick={_ => {
           this.actions().user.setStep({ step: 3 })
         }} />
       case 3:
-        // страница показывающая что кошелек выбран и предлагающая забрать токены
+        // page with info about current wallet and button to claim tokens
         return <InitialPage
           {...commonData}
           onClick={_ => {
@@ -100,19 +101,19 @@ class Claim extends React.Component {
           }}
         />
       case 4:
-        // клэйминг токенов в процессе
+        // claiming is in process
         return <ClaimingProcessPage
           {...commonData}
           transactionId={transactionId}
         />
       case 5:
-        // клейминг завершен
+        // claiming finished successfully
         return <ClaimingFinishedPage
           {...commonData}
           transactionId={transactionId}
         />
       default:
-        // загрузка
+        // зloading
         return <Loading />
     }
   }
