93fd66bcd7efbdfbe6c509c0ea5c7f78d712ba53
Amir <amiromayer@gmail.com>
2019-04-29 17:23:32 +0500

    Added comments

--- contracts/contracts/Storage.sol
+++ contracts/contracts/Storage.sol
@@ -2,19 +2,22 @@ pragma solidity >= 0.5.6;
 
 contract Storage {
 
+    // Address of linkdrop sender
     address payable public SENDER;
 
-    // Indicates who the link has been claimed to
+    // Indicates who the link is claimed to
     mapping (address => address) public claimedTo;
 
-    // Indicates whether the link has been canceled
+    // Indicates whether the link is canceled or not
     mapping (address => bool) internal _canceled;
 
-    // Address where proxy will route functions
+    // Address of implementation contract, where proxy will route functions
     address payable public masterCopy;
 
+    // Indicates whether the initializer function has been called or not
     bool internal _initialized;
 
+    // Indicates whether the contract is paused or not
     bool internal _paused;
 
     // Events
