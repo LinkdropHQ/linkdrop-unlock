7a4e4f3009268bbaedd8ae3d0b1f7e2cd00e05a9
amiromayer <amiromayer@gmail.com>
2019-05-12 15:09:06 +0500

    Added the most basic shipit file

---
+++ server/deploy/shipitfile.js
@@ -0,0 +1,27 @@
+// shipitfile.js
+module.exports = shipit => {
+  // Load shipit-deploy tasks
+  require('shipit-deploy')(shipit)
+  shipit.initConfig({
+    default: {
+      deployTo: 'linkdrop/var/www/linkdrop-monorepo',
+      repositoryUrl: 'git@github.com:LinkdropProtocol/linkdrop-monorepo.git'
+    },
+    staging: {
+      servers: 'root@rinkeby.linkdrop.io',
+      branch: 'dev'
+    }
+  })
+
+  // Copy local config file to remote
+  shipit.task('copyConfig', async () => {
+    await shipit.copyToRemote(
+      '../../config/server.config.json',
+      'linkdrop/var/www/linkdrop-monorepo/current/config/server.config.json'
+    )
+  })
+
+  shipit.on('deployed', () => {
+    shipit.start('copyConfig')
+  })
+}
