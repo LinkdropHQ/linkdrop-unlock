f4cc82d6faab980b28bf7013a8b4fb313164f4be
amiromayer <amiromayer@gmail.com>
2019-05-13 19:15:45 +0500

    Added minor fix

--- scripts/src/claim_eth.js
+++ scripts/src/claim_eth.js
@@ -7,7 +7,7 @@ const queryString = require('query-string')
 
 const { jsonRpcUrl, host, receiverAddress } = config
 
-// Get params from generated link [output/linkdrop_erc20.csv]
+// Get params from generated link [output/linkdrop_eth.csv]
 const getUrlParams = async i => {
   const csvFilePath = path.resolve(__dirname, '../output/linkdrop_eth.csv')
   const jsonArray = await csvToJson().fromFile(csvFilePath)
