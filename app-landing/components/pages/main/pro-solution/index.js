0af73f6195973272a706f161cfddcfbd16611949
spacehaz <spacehaz@gmail.com>
2019-05-13 11:30:32 +0300

    improvements for styles

--- app-landing/components/pages/main/pro-solution/index.js
+++ app-landing/components/pages/main/pro-solution/index.js
@@ -7,20 +7,22 @@ import { LinkBlock } from 'components/pages/common'
 @translate('pages.main')
 class ProSolution extends React.Component {
   render () {
-    const { onClick, onClose } = this.props
+    const { onClose } = this.props
     return <LinkBlock title={this.t('titles.wantMoreLinks')}>
-      <div className={styles.close} onClick={_ => onClose && onClose()}>
-        <Icons.Close />
+      <div className={styles.container}>
+        <div className={styles.close} onClick={_ => onClose && onClose()}>
+          <Icons.Close />
+        </div>
+        <div className={styles.description}>{this.t('descriptions.massGenerate')}</div>
+        <div className={styles.list}>
+          {LIST_ITEMS.map(item => <div className={styles.listItem}>
+            <Icons.CheckSmall /> {this.t(`titles.${item}`)}
+          </div>)}
+        </div>
+        <Button href='http://linkdrop.io/request' target='_blank' className={styles.button}>
+          {this.t('buttons.joinWaitlist')}
+        </Button>
       </div>
-      <div className={styles.description}>{this.t('descriptions.massGenerate')}</div>
-      <div className={styles.list}>
-        {LIST_ITEMS.map(item => <div className={styles.listItem}>
-          <Icons.CheckSmall /> {this.t(`titles.${item}`)}
-        </div>)}
-      </div>
-      <Button className={styles.button} onClick={_ => onClick && onClick()}>
-        {this.t('buttons.joinWaitlist')}
-      </Button>
     </LinkBlock>
   }
 }
