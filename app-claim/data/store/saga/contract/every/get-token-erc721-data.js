3262006be479f25309169b4f3cdcab10971388f2
spacehaz <spacehaz@gmail.com>
2019-05-15 11:36:15 +0300

    addded fix for erc-721 double check

--- app-claim/data/store/saga/contract/every/get-token-erc721-data.js
+++ app-claim/data/store/saga/contract/every/get-token-erc721-data.js
@@ -1,22 +1,48 @@
 import { put, call } from 'redux-saga/effects'
-import { getTokenERC721Data } from 'data/api/tokens'
+import { getERC721TokenData, getERC721MetaData } from 'data/api/tokens'
+import { ethers } from 'ethers'
+import NFTMock from 'contracts/NFTMock.json'
+import { defineNetworkName } from 'linkdrop-commons'
 
 const generator = function * ({ payload }) {
+  let image
+  let name
+  const { nftAddress, networkId, tokenId } = payload
   try {
     yield put({ type: 'CONTRACT.SET_LOADING', payload: { loading: true } })
-    const { nftAddress, networkId } = payload
-    const token = yield call(getTokenERC721Data, { tokenAddress: nftAddress, networkId })
-    if (token.symbol) {
-      yield put({ type: 'CONTRACT.SET_SYMBOL', payload: { symbol: token.symbol } })
+    const networkName = defineNetworkName({ networkId })
+    const provider = yield ethers.getDefaultProvider(networkName)
+    const nftContract = yield new ethers.Contract(nftAddress, NFTMock.abi, provider)
+    const metadataURL = yield nftContract.tokenURI(tokenId)
+    const data = yield call(getERC721TokenData, { erc721URL: metadataURL })
+    if (data) {
+      image = data.image
+      name = data.name
+    }
+    if (name) {
+      yield put({ type: 'CONTRACT.SET_SYMBOL', payload: { symbol: name } })
     }
 
-    yield put({ type: 'CONTRACT.SET_ICON', payload: { icon: token.featured_image_url || +(new Date()) } })
+    yield put({ type: 'CONTRACT.SET_ICON', payload: { icon: image || +(new Date()) } })
 
     yield put({ type: 'CONTRACT.SET_AMOUNT', payload: { amount: undefined } })
     yield put({ type: 'CONTRACT.SET_LOADING', payload: { loading: false } })
     yield put({ type: 'USER.SET_STEP', payload: { step: 1 } })
   } catch (e) {
-    console.error(e)
+    const token = yield call(getERC721MetaData, { tokenAddress: nftAddress, networkId })
+    if (token) {
+      image = token.featured_image_url
+      name = token.symbol
+    }
+    if (name) {
+      yield put({ type: 'CONTRACT.SET_SYMBOL', payload: { symbol: name } })
+    }
+
+    yield put({ type: 'CONTRACT.SET_ICON', payload: { icon: image || +(new Date()) } })
+
+    yield put({ type: 'CONTRACT.SET_AMOUNT', payload: { amount: undefined } })
+    yield put({ type: 'CONTRACT.SET_LOADING', payload: { loading: false } })
+    yield put({ type: 'USER.SET_STEP', payload: { step: 1 } })
   }
 }
 
