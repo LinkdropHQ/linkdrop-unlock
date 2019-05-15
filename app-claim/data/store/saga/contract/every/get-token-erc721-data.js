fea6989f4a0a79f8ee69ecdb03885f13084f6af4
spacehaz <spacehaz@gmail.com>
2019-05-15 12:32:27 +0300

    added fix for erc-721 token metadata

--- app-claim/data/store/saga/contract/every/get-token-erc721-data.js
+++ app-claim/data/store/saga/contract/every/get-token-erc721-data.js
@@ -8,17 +8,16 @@ const generator = function * ({ payload }) {
   try {
     yield put({ type: 'CONTRACT.SET_LOADING', payload: { loading: true } })
     let image = +(new Date())
-    let name = 'ERC-721 Token'
     const { nftAddress, networkId, tokenId } = payload
     const networkName = defineNetworkName({ networkId })
     const provider = yield ethers.getDefaultProvider(networkName)
     const nftContract = yield new ethers.Contract(nftAddress, NFTMock.abi, provider)
     const metadataURL = yield nftContract.tokenURI(tokenId)
+    const name = yield nftContract.symbol()
     if (metadataURL !== '') {
       const data = yield call(getERC721TokenData, { erc721URL: metadataURL })
       if (data) {
         image = data.image
-        name = data.name
       }
     }
     yield put({ type: 'CONTRACT.SET_SYMBOL', payload: { symbol: name } })
