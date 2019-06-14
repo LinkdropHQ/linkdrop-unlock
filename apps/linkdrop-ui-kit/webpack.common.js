b24c5391c900d00efd2265d218b99e4bd9629397
spacehaz <spacehaz@gmail.com>
2019-06-14 17:21:57 +0300

    added fix

--- apps/linkdrop-ui-kit/webpack.common.js
+++ apps/linkdrop-ui-kit/webpack.common.js
@@ -28,9 +28,7 @@ const postCSSLoader = {
     ident: 'postcss',
     sourceMap: true,
     plugins: () => [
-      autoprefixer({
-        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
-      })
+      autoprefixer()
     ]
   }
 }
