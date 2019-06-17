a1a5812cf7308a9e102e8049938111612a1ed360
spacehaz <spacehaz@gmail.com>
2019-06-17 14:33:20 +0300

    added fixes config check

--- apps/app-claim/webpack.prod.js
+++ apps/app-claim/webpack.prod.js
@@ -1,12 +1,9 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin()
-    ]
+    minimize: true
   }
 })
