726932275a1fac469cd598bfa55aa5be9972a60a
spacehaz <spacehaz@gmail.com>
2019-06-17 12:39:22 +0300

    added fixes config check

--- apps/app-demo/webpack.prod.js
+++ apps/app-demo/webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
