4ae31a5ca0de25d9f73adc86e44375c9d16fc9e8
Amir Jumaniyazov <amiromayer@gmail.com>
2019-07-04 18:46:07 +0500

    Added async handler

--- server/controllers/claimController.js
+++ server/controllers/claimController.js
@@ -83,17 +83,115 @@ export const claim = async (req, res) => {
     relayer
   )
 
+  const initcode = await proxyFactory.getInitcode()
+
+  const proxyAddress = await LinkdropSDK.computeProxyAddress(
+    factory,
+    linkdropMasterAddress,
+    initcode
+  )
+
+  // Check whether a claim tx exists in database
+  const oldClaimTx = await ClaimTx.findOne({
+    weiAmount,
+    tokenAddress,
+    tokenAmount,
+    expirationTime,
+    version,
+    chainId,
+    linkId,
+    linkdropMasterAddress,
+    receiverAddress
+  })
+  const table = new Table()
+  let type
+
+  if (oldClaimTx && oldClaimTx.txHash) {
+    if (tokenAddress === ethers.constants.AddressZero) type = 'ETH'
+    else {
+      if (weiAmount === '0') type = 'ERC20'
+      else type = 'ETH + ERC20'
+    }
+    table.push(['type', type])
+
+    table.push(['txHash', oldClaimTx.toObject().txHash])
+
+    term.green.bold(`\nSubmitted claim transaction\n`)
+    term.bold(table.toString(), '\n')
+
+    return res.json({
+      success: true,
+      txHash: oldClaimTx.txHash
+    })
+  }
+
   try {
-    const initcode = await proxyFactory.getInitcode()
+    let tx, txHash
 
-    const proxyAddress = await LinkdropSDK.computeProxyAddress(
-      factory,
-      linkdropMasterAddress,
-      initcode
-    )
+    // Top up pattern
+    if (isApprove == null || isApprove === 'false') {
+      // Check claim params
+      await proxyFactory.checkClaimParams(
+        weiAmount,
+        tokenAddress,
+        tokenAmount,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        proxyAddress
+      )
 
-    // Check whether a claim tx exists in database
-    const oldClaimTx = await ClaimTx.findOne({
+      // Claim
+      tx = await proxyFactory.claim(
+        weiAmount,
+        tokenAddress,
+        tokenAmount,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        { gasLimit: 500000 }
+      )
+    } else if (isApprove === 'true') {
+      // Approve pattern
+      // Check claim params
+      await proxyFactory.checkClaimParamsApprove(
+        weiAmount,
+        tokenAddress,
+        tokenAmount,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        proxyAddress
+      )
+
+      // Claim
+      tx = await proxyFactory.claimApprove(
+        weiAmount,
+        tokenAddress,
+        tokenAmount,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        { gasLimit: 500000 }
+      )
+    }
+
+    txHash = tx.hash
+
+    // Save claim tx to database
+    const claimTx = new ClaimTx({
       weiAmount,
       tokenAddress,
       tokenAmount,
@@ -102,142 +200,40 @@ export const claim = async (req, res) => {
       chainId,
       linkId,
       linkdropMasterAddress,
-      receiverAddress
+      receiverAddress,
+      proxyAddress,
+      txHash
     })
-    const table = new Table()
-    let type
 
-    if (oldClaimTx && oldClaimTx.txHash) {
-      if (tokenAddress === ethers.constants.AddressZero) type = 'ETH'
-      else {
-        if (weiAmount === '0') type = 'ERC20'
-        else type = 'ETH + ERC20'
+    const document = await claimTx.save()
+
+    if (tokenAddress === ethers.constants.AddressZero) type = 'ETH'
+    else {
+      if (weiAmount === '0') type = 'ERC20'
+      else type = 'ETH + ERC20'
+    }
+    table.push(['type', type])
+
+    for (let key in claimTx.toObject()) {
+      if (key !== '_id' && key !== '__v') {
+        table.push([key, claimTx.toObject()[key]])
       }
-      table.push(['type', type])
-
-      table.push(['txHash', oldClaimTx.toObject().txHash])
-
-      term.green.bold(`\nSubmitted claim transaction\n`)
-      term.bold(table.toString(), '\n')
-
-      return res.json({
-        success: true,
-        txHash: oldClaimTx.txHash
-      })
     }
 
-    try {
-      let tx, txHash
+    term.green.bold(`\nSubmitted claim transaction\n`)
+    term.bold(table.toString(), '\n')
 
-      // Top up pattern
-      if (isApprove == null || isApprove === 'false') {
-        // Check claim params
-        await proxyFactory.checkClaimParams(
-          weiAmount,
-          tokenAddress,
-          tokenAmount,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          proxyAddress
-        )
+    res.json({
+      success: true,
+      txHash: txHash
+    })
+  } catch (error) {
+    term.red.bold(`\n${error.reason ? error.reason : error}\n`)
 
-        // Claim
-        tx = await proxyFactory.claim(
-          weiAmount,
-          tokenAddress,
-          tokenAmount,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          { gasLimit: 500000 }
-        )
-      } else if (isApprove === 'true') {
-        // Approve pattern
-        // Check claim params
-        await proxyFactory.checkClaimParamsApprove(
-          weiAmount,
-          tokenAddress,
-          tokenAmount,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          proxyAddress
-        )
-
-        // Claim
-        tx = await proxyFactory.claimApprove(
-          weiAmount,
-          tokenAddress,
-          tokenAmount,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          { gasLimit: 500000 }
-        )
-      }
-
-      txHash = tx.hash
-
-      // Save claim tx to database
-      const claimTx = new ClaimTx({
-        weiAmount,
-        tokenAddress,
-        tokenAmount,
-        expirationTime,
-        version,
-        chainId,
-        linkId,
-        linkdropMasterAddress,
-        receiverAddress,
-        proxyAddress,
-        txHash
-      })
-
-      const document = await claimTx.save()
-
-      if (tokenAddress === ethers.constants.AddressZero) type = 'ETH'
-      else {
-        if (weiAmount === '0') type = 'ERC20'
-        else type = 'ETH + ERC20'
-      }
-      table.push(['type', type])
-
-      for (let key in claimTx.toObject()) {
-        if (key !== '_id' && key !== '__v') {
-          table.push([key, claimTx.toObject()[key]])
-        }
-      }
-
-      term.green.bold(`\nSubmitted claim transaction\n`)
-      term.bold(table.toString(), '\n')
-
-      res.json({
-        success: true,
-        txHash: txHash
-      })
-    } catch (error) {
-      term.red.bold(`\n${error.reason ? error.reason : error}\n`)
-
-      return res.json({
-        success: false,
-        error: error
-      })
-    }
-  } catch (err) {
-    term.red.bold(err)
+    return res.json({
+      success: false,
+      error: error
+    })
   }
 }
 
@@ -296,18 +292,110 @@ export const claimERC721 = async (req, res) => {
     relayer
   )
 
+  const initcode = await proxyFactory.getInitcode()
+
+  const proxyAddress = await LinkdropSDK.computeProxyAddress(
+    factory,
+    linkdropMasterAddress,
+    initcode
+  )
+
+  // Check whether a claim tx exists in database
+
+  const oldClaimTx = await ClaimTxERC721.findOne({
+    weiAmount,
+    nftAddress,
+    tokenId,
+    expirationTime,
+    version,
+    chainId,
+    linkId,
+    linkdropMasterAddress,
+    receiverAddress
+  })
+
+  const table = new Table()
+
+  if (oldClaimTx && oldClaimTx.txHash) {
+    table.push(['type', `${weiAmount === '0' ? 'ERC721' : 'ETH + ERC721'}`])
+    table.push(['txHash', oldClaimTx.toObject().txHash])
+
+    term.green.bold(`\nSubmitted claim transaction\n`)
+    term.bold(table.toString(), '\n')
+
+    return res.json({
+      success: true,
+      txHash: oldClaimTx.txHash
+    })
+  }
+
   try {
-    const initcode = await proxyFactory.getInitcode()
+    let tx, txHash
+    // Top up pattern
+    if (isApprove == null || isApprove === 'false') {
+      // Check claim params
+      await proxyFactory.checkClaimParamsERC721(
+        weiAmount,
+        nftAddress,
+        tokenId,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        proxyAddress
+      )
 
-    const proxyAddress = await LinkdropSDK.computeProxyAddress(
-      factory,
-      linkdropMasterAddress,
-      initcode
-    )
+      // Claim
 
-    // Check whether a claim tx exists in database
+      tx = await proxyFactory.claimERC721(
+        weiAmount,
+        nftAddress,
+        tokenId,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        { gasLimit: 500000 }
+      )
+    } else if (isApprove === 'true') {
+      // Approve pattern
+      // Check claim params
+      await proxyFactory.checkClaimParamsERC721Approve(
+        weiAmount,
+        nftAddress,
+        tokenId,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        proxyAddress
+      )
 
-    const oldClaimTx = await ClaimTxERC721.findOne({
+      // Claim
+
+      tx = await proxyFactory.claimERC721Approve(
+        weiAmount,
+        nftAddress,
+        tokenId,
+        expirationTime,
+        linkId,
+        linkdropMasterAddress,
+        linkdropSignerSignature,
+        receiverAddress,
+        receiverSignature,
+        { gasLimit: 500000 }
+      )
+    }
+    txHash = tx.hash
+
+    // Save claim tx to database
+    const claimTxERC721 = new ClaimTxERC721({
       weiAmount,
       nftAddress,
       tokenId,
@@ -316,129 +404,33 @@ export const claimERC721 = async (req, res) => {
       chainId,
       linkId,
       linkdropMasterAddress,
-      receiverAddress
+      receiverAddress,
+      proxyAddress,
+      txHash
     })
 
-    const table = new Table()
+    const document = await claimTxERC721.save()
 
-    if (oldClaimTx && oldClaimTx.txHash) {
-      table.push(['type', `${weiAmount === '0' ? 'ERC721' : 'ETH + ERC721'}`])
-      table.push(['txHash', oldClaimTx.toObject().txHash])
-
-      term.green.bold(`\nSubmitted claim transaction\n`)
-      term.bold(table.toString(), '\n')
-
-      return res.json({
-        success: true,
-        txHash: oldClaimTx.txHash
-      })
+    table.push(['type', `${weiAmount === '0' ? 'ERC721' : 'ETH + ERC721'}`])
+    for (let key in claimTxERC721.toObject()) {
+      if (key !== '_id' && key !== '__v') {
+        table.push([key, claimTxERC721.toObject()[key]])
+      }
     }
 
-    try {
-      let tx, txHash
-      // Top up pattern
-      if (isApprove == null || isApprove === 'false') {
-        // Check claim params
-        await proxyFactory.checkClaimParamsERC721(
-          weiAmount,
-          nftAddress,
-          tokenId,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          proxyAddress
-        )
+    term.green.bold(`\nSubmitted claim transaction\n`)
+    term.bold(table.toString(), '\n')
 
-        // Claim
+    res.json({
+      success: true,
+      txHash: tx.hash
+    })
+  } catch (error) {
+    term.red.bold(`\n${error.reason ? error.reason : error}\n`)
 
-        tx = await proxyFactory.claimERC721(
-          weiAmount,
-          nftAddress,
-          tokenId,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          { gasLimit: 500000 }
-        )
-      } else if (isApprove === 'true') {
-        // Approve pattern
-        // Check claim params
-        await proxyFactory.checkClaimParamsERC721Approve(
-          weiAmount,
-          nftAddress,
-          tokenId,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          proxyAddress
-        )
-
-        // Claim
-
-        tx = await proxyFactory.claimERC721Approve(
-          weiAmount,
-          nftAddress,
-          tokenId,
-          expirationTime,
-          linkId,
-          linkdropMasterAddress,
-          linkdropSignerSignature,
-          receiverAddress,
-          receiverSignature,
-          { gasLimit: 500000 }
-        )
-      }
-      txHash = tx.hash
-
-      // Save claim tx to database
-      const claimTxERC721 = new ClaimTxERC721({
-        weiAmount,
-        nftAddress,
-        tokenId,
-        expirationTime,
-        version,
-        chainId,
-        linkId,
-        linkdropMasterAddress,
-        receiverAddress,
-        proxyAddress,
-        txHash
-      })
-
-      const document = await claimTxERC721.save()
-
-      table.push(['type', `${weiAmount === '0' ? 'ERC721' : 'ETH + ERC721'}`])
-      for (let key in claimTxERC721.toObject()) {
-        if (key !== '_id' && key !== '__v') {
-          table.push([key, claimTxERC721.toObject()[key]])
-        }
-      }
-
-      term.green.bold(`\nSubmitted claim transaction\n`)
-      term.bold(table.toString(), '\n')
-
-      res.json({
-        success: true,
-        txHash: tx.hash
-      })
-    } catch (error) {
-      term.red.bold(`\n${error.reason ? error.reason : error}\n`)
-
-      return res.json({
-        success: false,
-        error: error
-      })
-    }
-  } catch (err) {
-    term.red.bold(err)
+    return res.json({
+      success: false,
+      error: error
+    })
   }
 }
