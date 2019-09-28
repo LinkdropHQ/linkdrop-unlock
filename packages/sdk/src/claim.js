612b5dbb25c1300e3c5d822d26cd24b0530e289a
Amir Jumaniyazov <amiromayer@gmail.com>
2019-09-28 13:05:51 +0500

    Updated sdk for unlock

--- packages/sdk/src/claim.js
+++ packages/sdk/src/claim.js
@@ -247,3 +247,119 @@ export const cancelLink = async ({
   const { error, errors, success, claimOperation } = response.data
   return { error, errors, success, claimOperation }
 }
+
+// ===========================================================================================
+//                                  UNLOCK
+// ===========================================================================================
+
+export const claimUnlock = async ({
+  jsonRpcUrl,
+  apiHost,
+  weiAmount,
+  tokenAddress,
+  tokenAmount,
+  expirationTime,
+  version,
+  chainId,
+  linkKey,
+  linkdropMasterAddress,
+  linkdropSignerSignature,
+  receiverAddress,
+  factoryAddress,
+  campaignId,
+  lock
+}) => {
+  if (lock === null || lock === '') {
+    throw new Error('Please provide lock address')
+  }
+
+  if (jsonRpcUrl === null || jsonRpcUrl === '') {
+    throw new Error('Please provide json rpc url')
+  }
+
+  if (apiHost === null || apiHost === '') {
+    throw new Error('Please provide api host')
+  }
+
+  if (weiAmount === null || weiAmount === '') {
+    throw new Error('Please provide amount of eth to claim')
+  }
+
+  if (tokenAddress === null || tokenAddress === '') {
+    throw new Error('Please provide ERC20 token address')
+  }
+
+  if (tokenAmount === null || tokenAmount === '') {
+    throw new Error('Please provide amount of tokens to claim')
+  }
+
+  if (expirationTime === null || expirationTime === '') {
+    throw new Error('Please provide expiration time')
+  }
+
+  if (version === null || version === '') {
+    throw new Error('Please provide mastercopy version ')
+  }
+
+  if (chainId === null || chainId === '') {
+    throw new Error('Please provide chain id')
+  }
+
+  if (linkKey === null || linkKey === '') {
+    throw new Error('Please provide link key')
+  }
+
+  if (linkdropMasterAddress === null || linkdropMasterAddress === '') {
+    throw new Error('Please provide linkdropMaster address')
+  }
+
+  if (linkdropSignerSignature === null || linkdropSignerSignature === '') {
+    throw new Error('Please provide linkdropMaster signature')
+  }
+
+  if (receiverAddress === null || receiverAddress === '') {
+    throw new Error('Please provide receiver address')
+  }
+
+  if (campaignId === null || campaignId === '') {
+    throw new Error('Please provide campaign id')
+  }
+
+  if (factoryAddress === null || factoryAddress === '') {
+    throw new Error('Please provide factory address')
+  }
+
+  // Get provider
+  const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl)
+
+  // Get receiver signature
+  const receiverSignature = await signReceiverAddress(linkKey, receiverAddress)
+
+  // Get linkId from linkKey
+  const linkId = new ethers.Wallet(linkKey, provider).address
+
+  const claimParams = {
+    weiAmount,
+    tokenAddress,
+    tokenAmount,
+    expirationTime,
+    version,
+    chainId,
+    linkId,
+    linkdropMasterAddress,
+    linkdropSignerSignature,
+    receiverAddress,
+    receiverSignature,
+    factoryAddress,
+    campaignId,
+    lock
+  }
+
+  const response = await axios.post(
+    `${apiHost}/api/v1/linkdrops/claimUnlock`,
+    claimParams
+  )
+
+  const { error, errors, success, txHash } = response.data
+  return { error, errors, success, txHash }
+}
